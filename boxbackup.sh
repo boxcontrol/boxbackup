a0c37f1a3544c5192f043548f975b30852f431ac
boxcontrol <boxcontrol.net@gmail.com>
2013-07-16 15:57:01 +0100

    Create boxbackup.sh

---
+++ boxbackup.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+####################################
+#
+# Backup to http://www.box.com/
+#
+####################################
+
+# What to backup.
+backup_files="/home /var/spool/mail /etc /root /boot /opt"
+
+# Where to backup to.
+dest="/home/path/to/box"
+
+# Create archive filename.
+day=$(date +%A)
+hostname=$(hostname -s)
+archive_file="$hostname-$day.tgz"
+
+# Print start status message.
+echo "Backing up $backup_files to $dest/$archive_file"
+date
+echo
+
+# Backup the files using tar.
+tar czf $dest/$archive_file $backup_files
+
+# Print end status message.
+echo
+echo "Backup finished"
+dat
